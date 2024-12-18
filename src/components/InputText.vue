f46be4c2c668ada39897b246004cbfa2bac78e3b
sh <ksh93813@gmail.com>
2024-12-18 16:03:23 +0900

    InputText 컴포넌트에 클리어 버튼 추가
    - 입력값이 있을 때 나타나는 클리어(X) 버튼 추가
    - 클리어 후 자동 포커스 기능 구현
    - 기존 입력 필드의 모든 상태별 테두리 스타일 유지
    - 테두리에 영향을 주지 않도록 버튼 내부 배치

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 type InputTextProps = {
   label: string;
 };
+
 const model = defineModel<string>();
 defineProps<InputTextProps>();
+
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const handleClear = () => {
+  model.value = '';
+  inputRef.value?.focus();
+};
 </script>
 
 <template>
   <label class="flex flex-col gap-4">
     {{ label }}
-    <input
-      class="border-gray-400 border rounded-md h-10 px-4 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none"
-      type="text"
-      autocomplete="off"
-      v-model="model"
-    />
+    <div class="relative">
+      <input
+        ref="inputRef"
+        class="w-full border-gray-400 border rounded-md h-10 px-4 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none"
+        type="text"
+        autocomplete="off"
+        v-model="model"
+      />
+      <button
+        v-if="model"
+        type="button"
+        class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+        @click="handleClear"
+      >
+        ✕
+      </button>
+    </div>
   </label>
 </template>
